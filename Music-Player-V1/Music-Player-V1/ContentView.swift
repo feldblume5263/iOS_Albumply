f4e131a9d49411e1ba8b124dd9e68ba846234847
feldblume5263 <feldblume5263@gmail.com>
2022-01-13 14:44:01 +0900

    feat: #01 print albums titles at VGrid 그리드에 라이브러리 title 출력
    이후 아티스트, 앨범 표지 출력 필요

--- Music-Player-V1/Music-Player-V1/ContentView.swift
+++ Music-Player-V1/Music-Player-V1/ContentView.swift
@@ -8,17 +8,47 @@
 import SwiftUI
 import MediaPlayer
 
-// 라이브러리 앨범 긁어서 반환
-func getLibraryAlbums() -> [MPMediaItemCollection]? {
-    guard let libraryAlbums = MPMediaQuery.albums().collections
-    else { return nil }
-    return libraryAlbums
+
+struct LibraryAlbum {
+    
+//    var albumID: String
+//    var albumImage: UIImage
+    var albumTitle: String
+}
+
+class LibraryAlbumsViewModel: ObservableObject {
+    
+    private var libraryAlbumsItemCollection: [MPMediaItemCollection]?
+    @Published var libraryAlbums = [LibraryAlbum]()
+    
+    init() {
+        setLibraryAlbumsQuery()
+        makeLibraryAlbumsArray()
+    }
+    
+    private func makeLibraryAlbumsArray() {
+        libraryAlbumsItemCollection?.forEach({ libraryAlbumItemCollection in
+            let newAlbumTitle = libraryAlbumItemCollection.representativeItem?.albumTitle
+            let newLibraryAlbum = LibraryAlbum(albumTitle: newAlbumTitle ?? "Undefined")
+            libraryAlbums.append(newLibraryAlbum)
+        })
+    }
+    
+    private func setLibraryAlbumsQuery() {
+        if let libraryAlbums = MPMediaQuery.albums().collections {
+            self.libraryAlbumsItemCollection = libraryAlbums
+        } else {
+            self.libraryAlbumsItemCollection = nil
+        }
+    }
+    // 라이브러리 앨범 개수 반환
+    func getLibraryAlbumsCount() -> Int {
+        return libraryAlbums.count
+    }
 }
 
 
 struct ContentView: View {
-    
-    @State var albumsCount: Int = 0
     
     var body: some View {
         NavigationView {
@@ -26,30 +56,24 @@ struct ContentView: View {
                 .padding(EdgeInsets(top: 50, leading: 0, bottom: 0, trailing: 0))
         }
     }
-    
-    private func setAlbumsCount() {
-        if let albums = MPMediaQuery.albums().collections {
-            albumsCount = albums.count
-        }
-    }
 }
 
 
 struct LibraryAlbumsView: View {
     
-    let columns = [
-        GridItem(.flexible()),
-        GridItem(.flexible())
-    ]
+    @ObservedObject var libraryAlbumsViewModel = LibraryAlbumsViewModel()
     
+    let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 2)
     var body: some View {
         ScrollView {
-            LazyVGrid(columns: columns) {
-                ForEach(0 ... 100, id: \.self) { _ in
-                    Color.orange
-                        .padding()
-                }
-            }.navigationTitle("라이브러리")
+            LazyVGrid(columns: columns, spacing: 30) {
+                ForEach(0 ..< libraryAlbumsViewModel.libraryAlbums.count, id: \.self) { libraryAlbumIndex in
+                    VStack {
+                        Text(libraryAlbumsViewModel.libraryAlbums[libraryAlbumIndex].albumTitle)
+                    }
+                }.background(Color.green)
+            }
+            .navigationTitle("라이브러리")
         }
     }
 }
